c89651803a59d3638570a7d8be41607742b51c6c
DomingoWong <yhuang158@dons.usfca.edu>
2019-01-03 23:52:49 -0800

    Add LeetCode 0001

--- LeetCode/0001TwoSum/Solution.java
+++ LeetCode/0001TwoSum/Solution.java
@@ -3,7 +3,7 @@ class Solution {
         int i = 0;
         int j = 0;
         for(i = 0; i < nums.length; i++) {
-            for(j=i+1; j<nums.length; j++) {
+            for(j = i+1; j < nums.length; j++) {
                 if(nums[i] + nums[j] == target) {
                     int result[] = {i, j};
                     return result;
